e147785b920cf47cf232dcb746ae665dbe67cd92
Parcos7 <mpribeiro63@gmail.com>
2025-03-26 13:17:02 -0300

    Update DadosItens.py

--- DadosItens.py
+++ DadosItens.py
@@ -1,15 +1,17 @@
 class DadosItens:
-    def __init__(self, sabor, qnt, valor):
+    def __init__(self, sabor, qnt, valor, saldo):
         self.sabor = sabor
         self.qnt = qnt
         self.valor = valor
+        self.saldo = saldo
         
     def ApDados(self):
-        print(f"\bNome: {self.sabor}\nQnt: {self.qnt}\nValor total por qnt: {self.valor*self.qnt}\n ")
+        print(f"\bNome: {self.sabor}\nQnt: {self.qnt}\nValor total por qnt: R$ {self.valor*self.qnt}\n ")
 
     def venda(self, qntVendida):
         if self.qnt >= qntVendida:
             self.qnt -= qntVendida
+            self.saldo += self.valor * qntVendida
             print(f"Venda realizada! Agora restam {self.qnt} unidades de {self.sabor}.\n")
         else:
             print("Estoque insuficiente!")
